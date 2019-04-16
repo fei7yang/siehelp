93c37cc740c612bb527579b4c051740ce353853c
fei7yang <fei7yang@163.com>
2019-04-16 18:19:44 +0800

    dont know what

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -4,11 +4,13 @@
 
 #import "GeneratedPluginRegistrant.h"
 #import <flutter_webview_plugin/FlutterWebviewPlugin.h>
+#import <fluttertoast/FluttertoastPlugin.h>
 
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
   [FlutterWebviewPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterWebviewPlugin"]];
+  [FluttertoastPlugin registerWithRegistrar:[registry registrarForPlugin:@"FluttertoastPlugin"]];
 }
 
 @end
